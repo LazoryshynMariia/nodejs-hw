193a4c2fe22f53888c468b546def20284a8daaa1
Mariia <ledimoon21@gmail.com>
2026-05-31 01:16:05 +0300

    setup mongodb and notes architecture

---
+++ src/models/note.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+const noteShema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        content: {
+            type: String,
+            required: false,
+            default: true,
+            trim: true,
+        },
+        tag: {
+            type: String,
+            enum: ["Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"],
+            required: false,
+            default: "Todo",
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+export const Note = model('Note', noteShema);
