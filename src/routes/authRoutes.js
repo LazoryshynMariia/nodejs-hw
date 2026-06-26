699b625dc1309ee600c0f9702b57bae7af63b975
Mariia <ledimoon21@gmail.com>
2026-06-26 04:16:26 +0300

    rename authController.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { celebrate } from "celebrate";
 import { Router } from "express";
 import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
-import { loginUser, logoutUser, refreshUserSession, registerUser } from "../controllers/authControllers.js";
+import { loginUser, logoutUser, refreshUserSession, registerUser } from "../controllers/authController.js";
 
 const authRouter = Router();
 
